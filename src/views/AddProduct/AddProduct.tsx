b769c32424e7b76abc75dee279ac3f35e78b4fdc
waaseyzulfiqar <wasey03112920792@gmail.com>
2024-07-26 22:59:01 +0500

    completed

--- src/views/AddProduct/AddProduct.tsx
+++ src/views/AddProduct/AddProduct.tsx
@@ -22,19 +22,13 @@ function AddProduct() {
   }
 
   return (
-    <div className="px-10 w-full h-min-screen py-6 bg-gray-200">
-      <h2 className="font-bold text-4xl mb-3 text-gray-700">Add Product</h2>
-      <p className="font-bold text-lg pb-2 text-gray-600">
-        Here you can add product to your store.
-      </p>
-
-      <div className="border-b border-gray-400"></div>
-
-      <div className="bg-white mt-4 px-8 py-3 w-full rounded-md">
-        <h3 className="font-medium tracking-wider text-2xl pt-3 text-gray-700 uppercase">
+    <div className="px-10 w-full h-min-screen py-6">
+  
+      <div className="bg-white mt-2 px-8 py-3 w-full rounded-md">
+        <h3 className="font-medium tracking-wider text-2xl text-gray-700 uppercase text-center">
           Product Details
         </h3>
-        <p className="font-bold text-base tracking-wider mb-9 text-gray-700">
+        <p className="font-bold text-base tracking-wider mb-16 text-gray-700 text-center">
           Add product details here
         </p>
 
@@ -72,14 +66,14 @@ function AddProduct() {
         </label>
         <input
           onChange={(e) => setPrice(e.target.value)}
-          className="block mb-7 border-2 border-gray-300 px-3 py-3 w-full rounded-md"
+          className="block mb-10 border-2 border-gray-300 px-3 py-3 w-full rounded-md"
           type="number"
           placeholder="Rs."
         />
 
         <button
         onClick={onSubmit}
-        className="border px-7 py-2 mb-4 font-medium rounded-md hover:bg-black hover:text-white">
+        className="border w-full px-7 py-2 font-medium rounded-md bg-blue-500 hover:bg-blue-400 text-white">
           Add
         </button>
       </div>
